57c9e6bc8063552974086059a55fcd84d5f2bd4b
Irina <irasheresh@gmail.com>
2022-12-18 21:12:46 +0100

    adding POST in serializers.py

--- genedata/api.py
+++ genedata/api.py
@@ -11,7 +11,19 @@ from .serializers import *
 def gene_detail(request, pk):
     """
     Retrieve, update or delete a code snippet.
+   
     """
+    #Our serializer needs to handle Foreig keys from Gene model
+    #sequencing = models.ForeignKey(Sequencing, on_delete=models.DO_NOTHING)
+    #ec = models.ForeignKey(EC, on_delete=models.DO_NOTHING)
+    if request.method == 'POST':
+        serializer = GeneSerializer(data=request.data)
+        #test wether the user data is corect respectivly to db model
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     try:
         gene = Gene.objects.get(pk=pk)
     except Gene.DoesNotExist:
--- genedata/serializers.py
+++ genedata/serializers.py
@@ -7,11 +7,37 @@ from .models import *
     # entity = serializers.CharField(required=True, allow_blank=False, max_length=256)
     # start = serializers.IntegerField()
 
+
+class ECSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EC
+        fields = ['id', 'ec_name']
+
+class SequencingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sequencing
+        fields = ['id', 'sequencing_factory', 'factory_location']
+
+
 class GeneSerializer(serializers.ModelSerializer):
+    ec = ECSerializer()
+    sequencing = SequencingSerializer()
     class Meta:
         model = Gene
-        fields = ['gene_id', 'entity', 'start', 'stop', 'sense', 'start_codon']
-
+        fields = ['gene_id', 'entity', 'start', 'stop', 'sense', 'start_codon', 'ec', 'sequencing']
+    # data that user will provide for ec and sequencing is not stored in a gene table 
+    # rewrite default save() to make input handled appropriately
+    def create(self, validated_data):
+        ec_data = self.initial_data.get('ec')
+        seq_data = self.initial_data.get('sequencing')
+        #we want to correctly link these foreign key fields in the gene table to the records in the correct EC or sequencing table
+        # so we look up at row in the EC table based on the data that I got from the user.
+        gene = Gene(**{**validated_data,
+                    'ec' : EC.objects.get(pk=ec_data['id']),
+                    'sequencing': Sequencing.objects.get(pk=seq_data['id']) 
+                    })
+        gene.save()
+        return gene
 
 
 class GeneListSerializer(serializers.ModelSerializer):
