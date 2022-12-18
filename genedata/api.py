9e10be9e6727e43a0180f36e14ace743d6979b2c
Irina <irasheresh@gmail.com>
2022-12-18 20:36:52 +0100

    Refactoring serializers with api_view(rest_framework.decorators)

--- genedata/api.py
+++ genedata/api.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -8,7 +7,7 @@ from rest_framework import status
 from .models import *
 from .serializers import *
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def gene_detail(request, pk):
     """
     Retrieve, update or delete a code snippet.
@@ -19,15 +18,21 @@ def gene_detail(request, pk):
         return HttpResponse(status=404)
     if request.method == 'GET':
         serializer = GeneSerializer(gene)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
 
-@csrf_exempt
+
+
+@api_view(['GET'])
 def genes_list(request):
-    if request.method == 'GET':
+    try:
         gene = Gene.objects.all()
+    except Gene.DoesNotExist:
+        return HttpResponse(status=404)
+    if request.method == 'GET':      
+        gene = Gene.objects.all()  
         serializer = GeneListSerializer(gene, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
 
 
